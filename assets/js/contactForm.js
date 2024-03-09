4a5c9890825d492a25e76bef85f6b41f96aa6485
Tyrone <145229174+Ty-Thetravelguy@users.noreply.github.com>
2024-03-09 09:32:11 +0000

    added Alert to notify the message was sent and then clears the form.

--- assets/js/contactForm.js
+++ assets/js/contactForm.js
@@ -4,16 +4,20 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     event.preventDefault();
 
     const formData = {
-        from_name: document.getElementById('name').value,
-        from_email: document.getElementById('email').value,
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
         message: document.getElementById('message').value
     };
 
-    emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData)
+    emailjs.send('service_7apagiv', 'template_2e0qu9m', formData)
         .then(function (response) {
+            alert('Message sent')
             console.log('Email sent successfully!', response);
+            document.getElementById('contact-form').reset();
             // Display a success message
         }, function (error) {
+            alert('Error, your message was not sent. Please try again!')
             console.error('Error sending email:', error);
             // Display error message if there is one
         });
